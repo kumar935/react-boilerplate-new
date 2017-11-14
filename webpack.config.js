4e6c88a3bfad9291b1d913c40cf9e5bbc62c3eaf
Animesh Kumar <Animesh@LAYAMEXIM07.local>
2017-11-14 17:58:03 +0530

    dependencies version updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const Webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   context: __dirname,
@@ -43,6 +44,11 @@ module.exports = {
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'),
+        include: path.join(__dirname, '/src'),
       },
     ],
   },
